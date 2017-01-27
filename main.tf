93d6536600a15aa326d10b1a7a273586ea0c0f0a
ilusha1975 <ilusha1975@yahoo.com>
2017-01-27 13:19:38 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -50,7 +50,7 @@ resource "aws_instance" "web" {
   tags = {
     Identity      = "autodesk-bass"
     SomeOtherTag  = "some other tag value"
-    YetAnotherTag = "Whatever2"
+    YetAnotherTag = "Whatever3"
     Name = "web ${count.index + 1}/${var.num_webs}"
   }
 }
